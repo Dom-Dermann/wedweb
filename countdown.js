b13effc87e6a8aa81cc43ce0866391ad55e0d2ba
Dominikus Dermannn <dominikdermann@gmail.com>
2023-05-15 18:16:42 +0200

    Update countdown.js

---
+++ countdown.js
@@ -0,0 +1,17 @@
+const countdown = () => {
+  const weddingDate = new Date('2023-10-13T12:00:00Z').getTime();
+  const now = new Date().getTime();
+  const difference = weddingDate - now;
+
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+  document.getElementById('days').innerHTML = days;
+  document.getElementById('hours').innerHTML = hours;
+  document.getElementById('minutes').innerHTML = minutes;
+  document.getElementById('seconds').innerHTML = seconds;
+};
+
+setInterval(countdown, 1000);
